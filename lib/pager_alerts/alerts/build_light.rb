d3d4eb49b8eab9ae1c5ee58ac7fef6a96a2de6d6
Ash McKenzie <ash@greenworm.com.au>
2013-08-15 10:43:56 +1000

    Write out state

--- lib/pager_alerts/alerts/build_light.rb
+++ lib/pager_alerts/alerts/build_light.rb
@@ -12,16 +12,18 @@ module PagerAlerts
           sleep 0.5
         end
 
-        # Can't get previous state ?
-        #
-        light.warning!
+        write_out_state
       end
 
       private
 
-      def light
-        @blinky ||= Blinky.new.light
-      end
+        def light
+          @blinky ||= Blinky.new.light
+        end
+
+        def write_out_state
+          File.open('/home/dev/bl/last_status', 'w') {|f| f.write('alert') }
+        end
     end
   end
 end
